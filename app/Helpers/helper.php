46d352a7a8cd1ff24094dbb972b7bdd22a1628db
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2024-03-10 10:44:21 +0100

    Update helper.php

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -173,7 +173,7 @@ if (! function_exists('socket')) {
             curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
             curl_close($curl);
             $response = json_decode(curl_exec($curl));
-            // info(json_encode($response));
+            info(json_encode($response));
             if (isset($response->status) && isset($response->message)) {
                 if ($response->status == true) {
                     $data = [
