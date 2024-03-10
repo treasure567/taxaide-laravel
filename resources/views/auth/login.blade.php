a30dec820d75677055f7e4f4c3c28959e71b0b61
Treasure Uvietobore <uvietoboretreasure@gmail.com>
2024-03-10 10:44:22 +0100

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -43,5 +43,10 @@
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
+        <div class="flex items-center justify-end mt-4">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                {{ __('Create an account') }}
+            </a>
+        </div>
     </form>
 </x-guest-layout>
